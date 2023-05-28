3ec01152fb5a7af019d0e1c28428eebcc00a1b1a
MainLoki <70030873+MainLoki@users.noreply.github.com>
2023-05-28 21:54:14 +0200

    .

--- ConnectFour/command.go
+++ ConnectFour/command.go
@@ -14,6 +14,7 @@ type TestCommand struct{}
 
 var _ provider.Command = TestCommand{}
 var permission int64 = discordgo.PermissionSendMessages
+var grid [6][7]int 
 
 func (TestCommand) CommandData() (discordgo.ApplicationCommand, error) {
 	return discordgo.ApplicationCommand{
@@ -56,15 +57,89 @@ func (TestCommand) CommandData() (discordgo.ApplicationCommand, error) {
 }
 
 func (TestCommand) Execute(proxy provider.ExecuteProxy) error {
-	file, _ := os.CreateTemp("*.png")
+	file, _ := os.CreateTemp(os.TempDir(), "*.png")
+	logger.Info(file.Name())
+	checkWin()
 	proxy.Respond(provider.Response{ Content: "test"}, false, false, false)
 	return nil
 }
 
-func generateImg(color color.RGBA, grid [][]string, file *os.File) image.RGBA {
-	img := image.NewRGBA(image.Rect(0, 0, , ))
+func generateImg(color color.RGBA, file *os.File) {
+	img := image.NewRGBA(image.Rect(0, 0, 0, 0))
 	
 	png.Encode(file, img)
+}
+
+func checkWin() bool {
+	return checkRows() || checkCols() || checkDiagonalsLeft() || checkDiagonalsRight()
+}
+
+func checkRows() bool {
+	for i := 0; i < 6; i++{
+		for j := 0; j < 7-3; j++ {
+			if grid[i][j] == grid[i][j+1] && grid[i][j+1] == grid[i][j+2] && grid[i][j+2] == grid[i][j+3] && grid[i][j+3] != 0 { return true }
+		}
+	}
+	return false
+}
+
+func checkCols() bool {
+	for i := 0; i < 7; i++{
+		for j := 0; j < 6-3; j++ {
+			if grid[j][i] == grid[j+1][i] && grid[j+1][i] == grid[j+2][i] && grid[j+2][i] == grid[j+3][i] && grid[j+3][i] != 0 { return true }
+		}
+	}
+	return false
+}
+
+func checkDiagonalsLeft() bool {
+	for i := 3; i > 0; i-- {
+		if fromUpperLeft(0, i) {
+			return true
+		}
+	} 
+	for i := 0; i < 3; i++ {
+		if fromUpperLeft(i, 0) {
+			return true
+		}
+	}
+	return false
+}
+
+func checkDiagonalsRight() bool {
+	for i := 3; i < 6; i++ {
+		if fromUpperRight(0, i) {
+			return true
+		}
+	} 
+	for i := 0; i < 3; i++ {
+		if fromUpperRight(i, 6) {
+			return true
+		}
+	}
+	return false
+}
+
+func fromUpperLeft(i,j int) bool {
+	for i+3 <= 5 && j+3 <= 6 {
+		if grid[i][j] == grid[i+1][j+1] && grid[i+1][j+1] == grid[i+2][j+2] && grid[i+2][j+2] == grid[i+3][j+3] && grid[i+3][j+3] != 0 {
+			return true
+		}
+		j++
+		i++
+	}
+	return false
+}
+
+func fromUpperRight(i,j int) bool {
+	for i+3 <= 5 && j-3 >= 0 {
+		if grid[i][j] == grid[i+1][j-1] && grid[i+1][j-1] == grid[i+2][j-2] && grid[i+2][j-2] == grid[i+3][j-3] && grid[i+3][j-3] != 0 {
+			return true
+		}
+		j--
+		i++
+	}
+	return false
 }
 
 
