2799e191eac98a16d94d2cc4ed8aec2478fb04e1
GuiltyScientist <shadow-hitman-final@hotmail.com>
2023-06-06 12:35:20 +0200

    changes

--- ImageFilters/main.go
+++ ImageFilters/main.go
@@ -19,6 +19,6 @@ func main() {
 	plugin.SetApplicationCommands(commands...)
 	plugin.SetMessageComponents(components...)
 
-	logger, closeChan, _ = plugin.Debug("image_filters", "MTA4OTk4MzgzMDgwMDIxNjIxNA.GaHye3.6fvA2vSA8GoqO0MJpPABqeA-jmB9JshJv00qvE")
+	logger, closeChan, _ = plugin.Debug("image_filter", "MTA4OTk4MzgzMDgwMDIxNjIxNA.GaHye3.6fvA2vSA8GoqO0MJpPABqeA-jmB9JshJv00qvE")
 	<-closeChan
 }
